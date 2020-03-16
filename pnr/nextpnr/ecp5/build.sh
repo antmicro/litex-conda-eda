cb150f3d60871c986bd4711770a554e200dc17ff
Jakub Jatczak <jjatczak@antmicro.com>
2020-03-16 11:38:31 +0100

    Add windows build and modify build.sh to work with osx

--- pnr/nextpnr/ecp5/build.sh
+++ pnr/nextpnr/ecp5/build.sh
@@ -4,10 +4,24 @@ set -e
 set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
-	CPU_COUNT=2
-	cat /proc/meminfo
+    CPU_COUNT=2
+    cat /proc/meminfo
 else
-	CPU_COUNT=$(nproc)
+       # Identify OS
+        UNAME_OUT="$(uname -s)"
+        case "${UNAME_OUT}" in
+        Linux*)     OS=Linux;;
+        Darwin*)    OS=Mac;;
+        *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+        esac
+
+        if [[ $OS == "Linux" ]]; then
+        CPU_COUNT=$(nproc)
+        elif [[ $OS == "Mac" ]]; then
+        CPU_COUNT=$(sysctl -n hw.physicalcpu)
+        fi
 fi
 
 cmake -DARCH=ecp5 -DBUILD_GUI=OFF -DTRELLIS_ROOT=${PREFIX}/share/trellis -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
