f45260b42d90d9fc0bdee23f4150105b00d558d0
Jakub Jatczak <jjatczak@antmicro.com>
2020-04-07 17:09:36 +0200

    Make verbose

--- syn/yosys-plugins/bld.bat
+++ syn/yosys-plugins/bld.bat
@@ -5,6 +5,8 @@ python -c "print(r'%PREFIX%'.replace('\\','/'))" > temp.txt
 set /p BASH_PREFIX=<temp.txt
 del temp.txt
 
+set INCLUDE_DIR=%BASH_PREFIX%/Library/include
+set LIBRARY_DIR=%BASH_PREFIX%/Library/lib
 echo PREFIX := %BASH_PREFIX%>Makefile.conf
 
 make -C fasm-plugin install
