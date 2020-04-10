7f2d5545ba80e089adc9858b0229b82e1bdab948
Jan Kowalewski <jkowalewski@antmicro.com>
2020-04-10 13:58:49 +0200

    pnr/nextpnr/ecp5: fix cmake configuration

--- pnr/nextpnr/ecp5/build.sh
+++ pnr/nextpnr/ecp5/build.sh
@@ -5,27 +5,15 @@ set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
     CPU_COUNT=2
-    cat /proc/meminfo
-else
-       # Identify OS
-        UNAME_OUT="$(uname -s)"
-        case "${UNAME_OUT}" in
-        Linux*)     OS=Linux;;
-        Darwin*)    OS=Mac;;
-        *)          OS="${UNAME_OUT}"
-                echo "Unknown OS: ${OS}"
-                exit;;
-        esac
-
-        if [[ $OS == "Linux" ]]; then
-        CPU_COUNT=$(nproc)
-        elif [[ $OS == "Mac" ]]; then
-        CPU_COUNT=$(sysctl -n hw.physicalcpu)
-        fi
 fi
 
-cmake -DARCH=ecp5 -DBUILD_GUI=OFF -DTRELLIS_ROOT=${PREFIX}/share/trellis -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
-make -k -j${CPU_COUNT} || true
-make
+cmake   -DARCH=ecp5 \
+        -DBUILD_GUI=OFF \
+        -DTRELLIS_INSTALL_PREFIX=${PREFIX} \
+        -DTRELLIS_ROOT=${PREFIX}/share/trellis \
+        -DCMAKE_INSTALL_PREFIX=/ \
+        -DENABLE_READLINE=No \
+        .
 
+make -k -j${CPU_COUNT} || true
 make DESTDIR=${PREFIX} install
