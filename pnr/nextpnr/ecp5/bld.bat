34b020ee782587ade8348c024fa18d022e2bede7
Jakub Jatczak <jjatczak@antmicro.com>
2020-06-04 17:56:52 +0200

    Fix bld.bat

--- pnr/nextpnr/ecp5/bld.bat
+++ pnr/nextpnr/ecp5/bld.bat
@@ -5,15 +5,15 @@ del temp.txt
 REM Install boost with vcpkg
 git clone --branch 2019.11 https://github.com/microsoft/vcpkg.git --single-branch
 cd vcpkg
-echo set(VCPKG_BUILD_TYPE release)>> triplets/x64-windows.cmake
+echo set(VCPKG_BUILD_TYPE release)>> triplets/x64-windows-static.cmake
 powershell -Command "bootstrap-vcpkg.bat -disableMetrics"
 if errorlevel 1 exit 1
-vcpkg install boost-filesystem:x64-windows boost-program-options:x64-windows boost-thread:x64-windows boost-python:x64-windows boost-dll:x64-windows eigen3:x64-windows
+vcpkg install boost-filesystem:x64-windows-static boost-program-options:x64-windows-static boost-thread:x64-windows-static boost-python:x64-windows-static boost-dll:x64-windows-static eigen3:x64-windows-static
 if errorlevel 1 exit 1
 
 cd ..
 
-cmake -DBUILD_GUI=OFF -DTRELLIS_ROOT="%BASH_PREFIX%/share/trellis" -DTRELLIS_INSTALL_PREFIX="%BASH_PREFIX%" -DARCH=ecp5 -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -A "x64" -DBUILD_SHARED=ON -DSTATIC_BUILD=OFF "-DCMAKE_INSTALL_PREFIX=%BASH_PREFIX%" .
+cmake -DBUILD_GUI=OFF -DTRELLIS_ROOT="%BASH_PREFIX%" -DPYTRELLIS_LIBDIR="%BASH_PREFIX%/lib/trellis" -DARCH=ecp5 -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_SHARED=OFF -DSTATIC_BUILD=ON "-DCMAKE_INSTALL_PREFIX=%BASH_PREFIX%" .
 if errorlevel 1 exit 1
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
