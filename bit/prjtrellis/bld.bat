ad2719b43e3c45555843e4ca05f3b4b8946000ff
Jakub Jatczak <jjatczak@antmicro.com>
2020-06-04 13:17:17 +0200

    Make build static again

--- bit/prjtrellis/bld.bat
+++ bit/prjtrellis/bld.bat
@@ -7,15 +7,15 @@ del temp.txt
 REM Install boost with vcpkg
 git clone --branch 2019.11 https://github.com/microsoft/vcpkg.git --single-branch
 cd vcpkg
-echo set(VCPKG_BUILD_TYPE release)>> triplets/x64-windows.cmake
+echo set(VCPKG_BUILD_TYPE release)>> triplets/x64-windows-static.cmake
 powershell -Command "bootstrap-vcpkg.bat -disableMetrics"
 if errorlevel 1 exit 1
-vcpkg install boost-filesystem:x64-windows boost-program-options:x64-windows boost-thread:x64-windows boost-python:x64-windows boost-dll:x64-windows
+vcpkg install boost-filesystem:x64-windows-static boost-program-options:x64-windows-static boost-thread:x64-windows-static boost-python:x64-windows-static boost-dll:x64-windows
 if errorlevel 1 exit 1
 
 REM Compile and install libtrellis
 cd ..\libtrellis
-cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DBUILD_PYTHON=ON -DBUILD_SHARED=ON -DSTATIC_BUILD=OFF "-DCMAKE_INSTALL_PREFIX=%BASH_PREFIX%" .
+cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_PYTHON=ON -DBUILD_SHARED=OFF -DSTATIC_BUILD=OON "-DCMAKE_INSTALL_PREFIX=%BASH_PREFIX%" .
 if errorlevel 1 exit 1
 find .. -name '*trellis*'
 cmake --build . --target install --config Release
