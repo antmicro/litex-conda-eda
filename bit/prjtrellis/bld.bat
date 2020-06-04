cb3731cfdb43f03284a521a830de4e78d4e8e939
Jakub Jatczak <jjatczak@antmicro.com>
2020-06-04 13:18:06 +0200

    Remove find command

--- bit/prjtrellis/bld.bat
+++ bit/prjtrellis/bld.bat
@@ -10,15 +10,15 @@ cd vcpkg
 echo set(VCPKG_BUILD_TYPE release)>> triplets/x64-windows-static.cmake
 powershell -Command "bootstrap-vcpkg.bat -disableMetrics"
 if errorlevel 1 exit 1
-vcpkg install boost-filesystem:x64-windows-static boost-program-options:x64-windows-static boost-thread:x64-windows-static boost-python:x64-windows-static boost-dll:x64-windows
+vcpkg install boost-filesystem:x64-windows-static boost-program-options:x64-windows-static boost-thread:x64-windows-static boost-python:x64-windows-static boost-dll:x64-windows-static
 if errorlevel 1 exit 1
 
 REM Compile and install libtrellis
 cd ..\libtrellis
 cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_PYTHON=ON -DBUILD_SHARED=OFF -DSTATIC_BUILD=OON "-DCMAKE_INSTALL_PREFIX=%BASH_PREFIX%" .
 if errorlevel 1 exit 1
-find .. -name '*trellis*'
 cmake --build . --target install --config Release
+if errorlevel 1 exit 1
 md %PREFIX%\lib\trellis
 cp %SRC_DIR%/Release/pytrellis.lib %PREFIX%/lib/trellis/
 cp %SRC_DIR%/Release/trellis.lib %PREFIX%/lib/trellis/
