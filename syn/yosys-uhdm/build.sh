3b8fc849fe4562d612e3fcbb9457f7c3ed0846ec
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-04 17:25:28 +0100

    uhdm-yosys: copy whole yosys install dir to conda prefix
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- syn/yosys-uhdm/build.sh
+++ syn/yosys-uhdm/build.sh
@@ -6,13 +6,11 @@ set -x
 export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
 export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
 export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
-export CC=gcc-${USE_SYSTEM_GCC_VERSION}
-export CXX=g++-${USE_SYSTEM_GCC_VERSION}
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib"
 
 cd Surelog && make PREFIX=$PWD/../image release install -j$CPU_COUNT && cd ..
-make PREFIX=$PWD/image install -j$CPU_COUNT
+make CONFIG=gcc PROGRAM_PREFIX=uhdm- PREFIX=$PWD/image install -j$CPU_COUNT
 
-mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX"
 
-cp "$SRC_DIR/image/bin/yosys" "$PREFIX/bin/yosys-uhdm"
+cp -r "$SRC_DIR"/image/* "$PREFIX/"
