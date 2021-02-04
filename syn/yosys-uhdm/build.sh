646acc3057aee814c5edc02ada4f4881c066ec4d
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-04 17:30:08 +0100

    Remove other then uhdm-yosys packages
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- syn/yosys-uhdm/build.sh
+++ syn/yosys-uhdm/build.sh
@@ -5,11 +5,9 @@ set -x
 
 export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
 export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
-export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib"
 
-cd Surelog && make PREFIX=$PWD/../image release install -j$CPU_COUNT && cd ..
-make CONFIG=gcc PROGRAM_PREFIX=uhdm- PREFIX=$PWD/image install -j$CPU_COUNT
+cd Surelog && make LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib" LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo" CONFIG=gcc CC=gcc-${USE_SYSTEM_GCC_VERSION} CXX=g++-${USE_SYSTEM_GCC_VERSION} PREFIX=$PWD/../image release install -j$CPU_COUNT && cd ..
+make LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib" LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo" CONFIG=gcc CC=gcc-${USE_SYSTEM_GCC_VERSION} CXX=g++-${USE_SYSTEM_GCC_VERSION} PROGRAM_PREFIX=uhdm- PREFIX=$PWD/image install -j$CPU_COUNT
 
 mkdir -p "$PREFIX"
 
