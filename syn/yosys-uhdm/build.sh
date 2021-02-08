4f96fa9fae417cdc18f90d97c839f8e50aa51bdc
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-08 14:32:40 +0100

    wip

--- syn/yosys-uhdm/build.sh
+++ syn/yosys-uhdm/build.sh
@@ -9,6 +9,9 @@ export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib"
 
 cd Surelog && make PREFIX=$PWD/../image release install -j$CPU_COUNT && cd ..
+
+alias gcc=x86_64-conda_cos6-linux-gnu-gcc
+alias gxx=x86_64-conda_cos6-linux-gnu-gcc
 make ENABLE_READLINE=0 CONFIG=conda-linux PROGRAM_PREFIX=uhdm- PREFIX=$PWD/image install -j$CPU_COUNT
 
 mkdir -p "$PREFIX/"
