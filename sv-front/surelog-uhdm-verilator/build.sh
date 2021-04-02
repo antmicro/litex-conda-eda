dc42a2b438272ead7ebb6ae8d00f5c23e5a44981
Kamil Rakoczy <krakoczy@antmicro.com>
2021-04-02 11:51:51 +0200

    Fix Surelog version in uhdm-integration packages
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

---
+++ sv-front/surelog-uhdm-verilator/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd Surelog
+make PREFIX=./image release install -j$CPU_COUNT
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/lib/surelog/sv"
+
+cp "$SRC_DIR/image/lib/surelog/sv/builtin.sv" "$PREFIX/lib/surelog/sv/builtin.sv"
+cp "$SRC_DIR/image/bin/surelog" "$PREFIX/bin/surelog-uhdm"
