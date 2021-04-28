0e26b9b97288297aa78e50ca29945e4fd445d835
Kamil Rakoczy <krakoczy@antmicro.com>
2021-04-28 13:56:43 +0200

    WIP: add missing flags
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- sv-front/surelog-uhdm-yosys/build.sh
+++ sv-front/surelog-uhdm-yosys/build.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
+export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
+export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib"
+
 cd Surelog
 make PREFIX=./image release install -j$CPU_COUNT
 
